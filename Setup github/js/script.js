fed76276efb3786657c7e803dfa5d047fa87d817
Keddon Campbell <mrkfed101@gmail.com>
2015-02-03 21:15:10 -0500

    Console Test
    assed the console .log functions to the script.js file

--- Setup github/js/script.js
+++ Setup github/js/script.js
@@ -4,4 +4,9 @@ Test JS file
 Date 2/3/15
  */
 
-alert("testing 1, 2, 3")
+//alert("testing 1, 2, 3")
+
+console.log("Testing the console.log");
+
+var userInput = prompt("Type in your name");
+console.log(userInput);
